e237298783a6cefb7eb21967b5727deddd049b6d
Mohamad_Salmeh <MohamadSalmeh@gmail.com>
2026-01-05 21:36:44 +0300

    fix regex

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -6,19 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
-    
+
     public function authorize(): bool
     {
         return true;
     }
 
-    // | regex:/^(?:\+9639\d{8}|09\d{8})$/
-    //  | regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/
+
     public function rules(): array
     {
         return [
             'email' => 'sometimes | string | email | between:11,255|required_without:phone ',
-            'phone' => 'sometimes|required_without:email',
+            'phone' => 'sometimes|required_without:email|regex:/^09[0-9]{8}$/',
             'password' => 'required | string | min: 8',
         ];
     }
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -16,17 +16,16 @@ class RegisterRequest extends FormRequest
         return [
             'name'     => 'required|string|between:8,255',
             'email'    => 'required|string|email|between:11,255|unique:users,email',
-            'phone' => 'required|unique:users,phone',
-            'password' => 'required|string|min:8|confirmed',
+            'phone' => 'required|unique:users,phone|regex:/^09[0-9]{8}$/',
+            'password' => 'required|string|min:8|confirmed|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/',
         ];
     }
-    //|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/
-    //|not_regex:/^[0-9]/
-//|regex:/^09[0-9]{8}$/
-   /* public function messages()
+
+
+    public function messages()
     {
         return [
             'password.regex' => 'The password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@ $ ! % * ? &).',
         ];
-    }*/
+    }
 }
